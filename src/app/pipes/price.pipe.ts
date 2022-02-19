da42d1ac2b80fe3b761434345b1b20fface09564
Debian <debian@vps-a8af6aae.vps.ovh.ca>
2022-02-19 17:17:55 +0000

    actualizacion para la matriz para los usuarios no admin

--- src/app/pipes/price.pipe.ts
+++ src/app/pipes/price.pipe.ts
@@ -1,13 +1,13 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({
-    name: 'priceUSDT'
+  name: 'priceUSDT',
 })
-
 export class priceUSDT implements PipeTransform {
-    public transform(item: any, replace, replacement): any {
-        if (item == null) return "";
-        item = item.replace(replace, replacement);
-        return item;
-    }
+  public transform(item: any, replace, replacement): any {
+    if (item == null) return '';
+    item = item.replace(replace, replacement);
+    return item;
+  }
 }
+
--- src/app/services/alertService/alert.service.ts
+++ src/app/services/alertService/alert.service.ts
@@ -1,42 +1,45 @@
 import { Injectable } from '@angular/core';
 import { ToastrService } from 'ngx-toastr';
 export enum from {
-  top = "top",
-  bottom = "bottom",
+  top = 'top',
+  bottom = 'bottom',
 }
 export enum aling {
-  left = "left",
-  center = "center",
-  right = "right",
+  left = 'left',
+  center = 'center',
+  right = 'right',
 }
 export enum status {
-  info = "info",
-  success = "success",
-  warning = "warning",
-  error = "error",
+  info = 'info',
+  success = 'success',
+  warning = 'warning',
+  error = 'error',
 }
 export interface iAlert {
-  form: from,
-  aling: aling,
-  status: status,
+  form: from;
+  aling: aling;
+  status: status;
 }
 @Injectable({
   providedIn: 'root',
 })
 export class AlertService {
-  constructor(private toastr: ToastrService) { }
+  constructor(private toastr: ToastrService) {}
   show(from: from, align: aling, status: status, mesagge: string) {
-    this.toastr[status](`
+    this.toastr[status](
+      `
 <div class="container p-0 m-0">
   <p class="text-center"> <i class="now-ui-icons yafuz-${status}"></i> <h3>${mesagge}</h3></p>
 </div>
-    `, '', {
-      timeOut: 10000,
-      closeButton: true,
-      enableHtml: true,
-      toastClass: `alert alert-${status} alert-with-icon`,
-      positionClass: 'toast-' + from + '-' + align
-    });
-
+    `,
+      '',
+      {
+        timeOut: 10000,
+        closeButton: true,
+        enableHtml: true,
+        toastClass: `alert alert-${status} alert-with-icon`,
+        positionClass: 'toast-' + from + '-' + align,
+      }
+    );
   }
 }
